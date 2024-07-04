127aa247344a5f1a38220668381333f83bd20b88
Junyoul Lee <201902817@hufs.ac.kr>
2024-07-04 15:01:30 +0900

    wiki

--- chatbot-master/chatbot_test.py
+++ chatbot-master/chatbot_test.py
@@ -1,8 +1,16 @@
 from chatbot import Chat, register_call
 import os
-
+import wikipedia
 import warnings
 warnings.filterwarnings("ignore")
+
+@register_call("Wiki")
+def who_is(session=None, query='South Korea'):
+    try:
+        return wikipedia.summary(query)
+    except Exception:
+        pass
+    return "I don't know about "+query
 
 
 @register_call("do_you_know")
